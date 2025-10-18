4a246a379ed1a02c207aeb979b7b85f7e2d07131
s.khakimov <s.khakimov@iva.ru>
2025-10-18 18:27:24 +0300

    кнопки

--- frontend/src/modules/profile/feature/list/list.component.ts
+++ frontend/src/modules/profile/feature/list/list.component.ts
@@ -1,32 +1,36 @@
 import {Component, input, signal} from '@angular/core';
 import {ButtonComponent} from "../../ui/button/button.component";
 import {ListItemComponent} from "../../ui/list-item/list-item.component";
+import {FeatureToggleDirective} from "../../../../utils/feature-toggle.directive";
+import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'profile-list',
     imports: [
         ButtonComponent,
-        ListItemComponent
+        ListItemComponent,
+        FeatureToggleDirective,
+        RouterLink
     ],
     templateUrl: './list.component.html',
     styleUrl: './list.component.less'
 })
 export class ListComponent {
     key = input.required<string>();
-    list = signal([
-        {
-            id: 1,
-            title: 'Химик-аналитик (Группа физико-химических методов)',
-            views: 562,
-            responses: 34,
-            conversion: 28
-        },
-        {
-            id: 2,
-            title: 'Химик-аналитик',
-            views: 562,
-            responses: 34,
-            conversion: 28
-        },
+    list = signal<{ id: string, title: string, views: number, responses: number, conversion: number }[]>([
+        // {
+        //     id: 1,
+        //     title: 'Химик-аналитик (Группа физико-химических методов)',
+        //     views: 562,
+        //     responses: 34,
+        //     conversion: 28
+        // },
+        // {
+        //     id: 2,
+        //     title: 'Химик-аналитик',
+        //     views: 562,
+        //     responses: 34,
+        //     conversion: 28
+        // },
     ])
 }
